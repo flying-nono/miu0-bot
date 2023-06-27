49b1f126f78abce058e10446aa0f86b973a3eb92
flying-nono <103546991+flying-nono@users.noreply.github.com>
2023-06-27 20:21:43 +0800

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-import os
-
-class Config(BaseModel):
-    plugin_dir:str = os.path.dirname(__file__)
-    
-
-# print(os.path.dirname(__file__))
-# /root/bot/miu0-bot/src/plugins/test2
