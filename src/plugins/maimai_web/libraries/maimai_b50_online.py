b6297af47571571dfd3b11111daa53cd3f2eb984
flying-nono <103546991+flying-nono@users.noreply.github.com>
2023-06-29 22:18:39 +0800

    Update maimai_b50_online.py

--- src/plugins/maimai_web/libraries/maimai_b50_online.py
+++ src/plugins/maimai_web/libraries/maimai_b50_online.py
@@ -1,18 +1,23 @@
 import nonebot
 
 from PIL import Image
+from typing import Union
 
 from fastapi import FastAPI, Request, Form
-from fastapi.responses import HTMLResponse, FileResponse
+from fastapi.responses import FileResponse
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 from .maimai_b50 import generate50
+from .maimai_api import _get_b50_json
+from .maimai_models import UserInfo
+from ..config import plugin_config
 
-from ..config import Config
-
-plugin_config = Config.parse_obj(nonebot.get_driver().config)
 
 app: FastAPI = nonebot.get_app()
+
+app.mount("/web", StaticFiles(directory=plugin_config.plugin_dir+"/web"))
+'''设置获取静态资源文件的路由和文件目录'''
 
 templates = Jinja2Templates(directory=plugin_config.plugin_dir+"/web")
     
@@ -23,7 +28,7 @@ async def index(request: Request):
     return templates.TemplateResponse("index.html",context=context)
 
 
-@app.post("/b50")
+@app.post("/b50/image")
 async def get_b50(qq:str=Form(''),username:str=Form('')):
     if qq != '':
         payload = {"qq":qq,'b50':True}
@@ -32,8 +37,7 @@ async def get_b50(qq:str=Form(''),username:str=Form('')):
         payload = {"username":username,'b50':True}
         value = username
     
-    name = "b50_online"
-    output, success = await generate50(payload,name)
+    output, success = await generate50(payload,value)
 
     if success == 400:
         return "未找到此玩家，请确保此玩家的用户名和查分器中的用户名相同。"
@@ -48,4 +52,21 @@ async def get_b50(qq:str=Form(''),username:str=Form('')):
 
     return FileResponse(b50_img_path)
 
+
+@app.post("/b50/trend")
+async def get_maitrend(qq:str=Form(''),username:str=Form('')):
+    if qq != '':
+        payload = {"qq":qq,'b50':True}
+        b50_json, success = await _get_b50_json(payload)
+        if success == 400:
+            return "未找到此玩家，请确保此玩家的用户名和查分器中的用户名相同。"
+        elif success == 403:
+            return "该用户禁止了其他人获取数据。"
+        username = b50_json["username"]
+    else:
+        pass
+
+    userinfo = UserInfo.find_user(username)
+    if userinfo.isnewuser:
+        return "bot的数据库中没有该玩家，请多用bot查几次b50吧"
 
