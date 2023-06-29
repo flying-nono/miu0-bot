98b676f36afb06dd47eb2fd6f553b207ff439e60
Tony <tony@gmail.com>
2023-06-29 23:18:20 +0800

    同步后端代码，完善文件结构

--- src/plugins/maimai_web/config.py
+++ src/plugins/maimai_web/config.py
@@ -1,9 +1,12 @@
+from nonebot import get_driver
 from pydantic import BaseModel
 import os
+
 
 class Config(BaseModel):
     plugin_dir:str = os.path.dirname(__file__)
     
+plugin_config = Config.parse_obj(get_driver().config)
 
 # print(os.path.dirname(__file__))
-# /root/bot/miu0-bot/src/plugins/test2
+# /root/bot/miu0-bot/src/plugins/maimai_web
